6bca6b95717ae31f21a9ab8eda23012b4fdab758
Ganesh280896 <ganesh_r@comcast.com>
2024-07-31 10:44:37 +0530

    Add files via upload

---
+++ src/components/testsuites.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+export const Testsuites = (props) => {
+  return (
+    <div id="testsuites">
+      <div className="container">
+        <div className="section-title text-center">
+          <h2>What our clients say</h2>
+        </div>
+        <div className="row">
+          {props.data
+            ? props.data.map((d, i) => (
+                <div key={`${d.name}-${i}`} className="col-md-4">
+                  <div className="testsuites">
+                    <div className="testimonial-image">
+                      {" "}
+                      <img src={d.img} alt="" />{" "}
+                    </div>
+                    <div className="testsuites-content">
+                      <p>"{d.text}"</p>
+                      <div className="testsuites-meta"> - {d.name} </div>
+                    </div>
+                  </div>
+                </div>
+              ))
+            : "loading"}
+        </div>
+      </div>
+    </div>
+  );
+};
